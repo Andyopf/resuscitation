1da384ecfdc53aebddb4823a6cc0b56e21c83e41
Anan Lappikulthong <ananlappikulthong@Anans-MacBook-Air.local>
2017-02-09 23:15:04 +0700

    3

--- Resuscitation2/RhythmVC1.swift
+++ Resuscitation2/RhythmVC1.swift
@@ -14,17 +14,36 @@ import AVFoundation
 class RhythmVC1: UIViewController {
     
 
+    @IBOutlet weak var checkRhythmLbl: UILabel!
     @IBOutlet weak var CPRTimer: UILabel!
-    @IBOutlet weak var cprBtn: UIButton!
-    @IBOutlet weak var startCPRLbl: UILabel!
+    @IBOutlet weak var twoMinsLbl: UILabel!
+    @IBOutlet weak var intervalLbl: UILabel!
     
-    var CPRTime = 12345
+    @IBOutlet weak var medicationBtn: UIButton!
+    
+    @IBOutlet weak var cprBtn: UIButton!
+    @IBOutlet weak var epinephrineBtn: UIButton!
+    @IBOutlet weak var amiodaroneBtn: UIButton!
+    @IBOutlet weak var defibrillationBtn: UIButton!
+    @IBOutlet weak var reverseBtn: UIButton!
+    @IBOutlet weak var resetBtn: UIButton!
+    
+    @IBOutlet weak var vfBtn: UIButton!
+    @IBOutlet weak var pvtBtn: UIButton!
+    @IBOutlet weak var asystoleBtn: UIButton!
+    @IBOutlet weak var peaBtn: UIButton!
+    
+    var CPRTime = 1234
     var cprInterval = 10
     var now = NSDate()
     var showDatePattern = DateFormatter()
     var convertedDate: String = ""
     var timer1: Timer!
     var timer2: Timer!
+    var timer3: Timer!
+    var numberOfCPR = 0
+    var countAmio = 0
+    var timeOfVT = 0
     
     var backgroundMusicPlayer: AVAudioPlayer!
     
@@ -35,7 +54,10 @@ class RhythmVC1: UIViewController {
         showDatePattern.dateFormat = "dd-MM-yy;HH:mm:ss"
         convertedDate = showDatePattern.string(from: now as Date)
         playMetronome()
-        
+        resetBtn.isHidden = true
+        defibrillationBtn.isHidden = true
+        checkRhythmLbl.isHidden = true
+        cprBtn.isHidden = true
         
     }
     
@@ -75,63 +97,167 @@ class RhythmVC1: UIViewController {
     }
     
     func CPRIntervalTime() {
-        cprInterval -= 1
+        if cprInterval > 0 {
+            cprInterval -= 1
+        } else if cprInterval == 0 {
+            checkRhythmLbl.isHidden = false
+            _ = Timer.scheduledTimer(timeInterval: 1.5, target: self, selector: #selector(RhythmVC1.checkEcg), userInfo: nil, repeats: false)
+            cprInterval = 10
+            ecgAppear()
+            epinephrineBtn.isHidden = true
+            defibrillationBtn.isHidden = true
+            
+        }
+        let seconds = cprInterval % 60
+        let minutes = cprInterval / 60
+        twoMinsLbl.text = String(format: "%02d:%02d", minutes, seconds)
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "rhythmToVF" {
-            let destination1_1VC : VFpVT = segue.destination as! VFpVT
-            let destination1_2VC : VFpVT = segue.destination as! VFpVT
-            destination1_1VC.CPRTime = CPRTime
-            destination1_2VC.CPRInterval = cprInterval
-        } else if segue.identifier == "rhythmToAsyst" {
-            let destination2_1VC : AsystolePEAVC = segue.destination as! AsystolePEAVC
-            let destination2_2VC : AsystolePEAVC = segue.destination as! AsystolePEAVC
-            destination2_1VC.CPRTime = CPRTime
-            destination2_2VC.CPRInterval = cprInterval
+    func ecgAppear() {
+        peaBtn.isHidden = false
+        asystoleBtn.isHidden = false
+        pvtBtn.isHidden = false
+        vfBtn.isHidden = false
+    }
+    
+    func ecgDisappear() {
+        peaBtn.isHidden = true
+        asystoleBtn.isHidden = true
+        pvtBtn.isHidden = true
+        vfBtn.isHidden = true
+    }
+    
+    func checkEcg() {
+        checkRhythmLbl.isHidden = true
+    }
+    
+    func toSummary() {
+        self.performSegue(withIdentifier: "ToSummary", sender: nil)
+    }
+    
+    func sequenceDefib() {
+        if timeOfVT == 1 {
+            // cpr only
+        } else if timeOfVT == 2 {
+            epinephrineBtn.isHidden = false
+        } else if timeOfVT > 2 {
+            epinephrineBtn.isHidden = false
+            amiodaroneBtn.isHidden = false
+            // give amiodarone and epinephrine
         }
     }
     
-    func rhythmToVf() {
-        self.performSegue(withIdentifier: "rhythmToVF", sender: nil)
-    }
-    
-    func rhythmToAsyst() {
-        self.performSegue(withIdentifier: "rhythmToAsyst", sender: nil)
+    func resetDelayed() {
+        resetBtn.isHidden = false
     }
     
     //ECG function buttons
     
     @IBAction func VFBtnPressed(_ sender: Any) {
-        rhythmToVf()
+        
         let post = Post(drugPath: "VF", timePath: convertedDate)
         DataService.instance.addPosts(post: post)
+        ecgDisappear()
+        defibrillationBtn.isHidden = false
+        cprBtn.isHidden = false
     }
     
     @IBAction func pVTBtnPressed(_ sender: Any) {
-        rhythmToVf()
+        
         let post = Post(drugPath: "pVT", timePath: convertedDate)
         DataService.instance.addPosts(post: post)
+        ecgDisappear()
+        defibrillationBtn.isHidden = false
+        cprBtn.isHidden = false
     }
     
     @IBAction func asystoleBtnPressed(_ sender: Any) {
-        rhythmToAsyst()
+        
         let post = Post(drugPath: "Asystole", timePath: convertedDate)
         DataService.instance.addPosts(post: post)
+        ecgDisappear()
+        epinephrineBtn.isHidden = false
+        cprBtn.isHidden = false
     }
     
     @IBAction func PEABtnPressed(_ sender: Any) {
-        rhythmToAsyst()
+        
         let post = Post(drugPath: "PEA", timePath: convertedDate)
         DataService.instance.addPosts(post: post)
+        ecgDisappear()
+        epinephrineBtn.isHidden = false
+        cprBtn.isHidden = false
     }
     
+    // CPR buttons
+    
     @IBAction func startCPRPressed(_ sender: Any) {
-        let post = Post(drugPath: "Start CPR", timePath: convertedDate)
-        DataService.instance.addPosts(post: post)
         cprCountDown()
         cprBtn.isHidden = true
-        startCPRLbl.isHidden = true
+        if numberOfCPR == 0 {
+            let post = Post(drugPath: "Start CPR", timePath: convertedDate)
+            DataService.instance.addPosts(post: post)
+            numberOfCPR += 1
+        } else {
+            let post = Post(drugPath: "CPR", timePath: convertedDate)
+            DataService.instance.addPosts(post: post)
+        }
+    }
+    
+    @IBAction func defibrillationPressed(_ sender: Any) {
+        defibrillationBtn.isHidden = true
+        let post = Post(drugPath: "defibrillation", timePath: convertedDate)
+        DataService.instance.addPosts(post: post)
+        timeOfVT += 1
+        sequenceDefib()
+        
+    }
+    
+    // drugs buttons
+    @IBAction func epinepAdministered(_ sender: Any) {
+        let post = Post(drugPath: "Epinephrine 1 mg", timePath: convertedDate)
+        DataService.instance.addPosts(post: post)
+        epinephrineBtn.isHidden = true
+    }
+    
+    @IBAction func cordaroneAdministered(_ sender: Any) {
+        amiodaroneBtn.isHidden = true
+        countAmio += 1
+        if countAmio == 1 {
+            let post = Post(drugPath: "Amiodarone 300 mg", timePath: convertedDate)
+            DataService.instance.addPosts(post: post)
+        } else {
+            let post = Post(drugPath: "Amiodarone 150 mg", timePath: convertedDate)
+            DataService.instance.addPosts(post: post)
+        }
+    }
+
+    @IBAction func stopCPRPressed(_ sender: Any) {
+        toSummary()
+        timer1.invalidate()
+        timer2.invalidate()
+        
+        cprBtn.isHidden = true
+        defibrillationBtn.isHidden = true
+        reverseBtn.isHidden = true
+        timer3 = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(RhythmVC1.resetDelayed), userInfo: nil, repeats: false)
+        
+        medicationBtn.isHidden = true
+        ecgDisappear()
+        twoMinsLbl.isHidden = true
+        intervalLbl.isHidden = true
+        
+        
+        backgroundMusicPlayer.stop()
+        
+    }
+    
+    @IBAction func resetBtnPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "goReset", sender: nil)
+    }
+    
+    @IBAction func reviewBtnPressed(_ sender: Any) {
+        toSummary()
     }
 
 
